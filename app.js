0fddd8c3c61eacd960dc6562288ab1e0be597a48
Abhay Tripathi <abhay.mani29@gmail.com>
2025-07-08 00:46:19 +0530

    all work done added responsiveness for all tablet and mobile and email alert to me on the form submission throught formspree and also made all the buttons and links  functional

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const video1 = document.querySelector('#projectVideo1');
 const video2 = document.querySelector('#projectVideo2');
 const video3 = document.querySelector('#projectVideo3');
 const hoverSign = document.querySelector('.hover-sign');
+//contact button element
+const contactBtn = document.querySelectorAll('.contact-btn');
+const contactSection = document.querySelector('#contact');
+ // side-bar element
+ const sideBar = document.querySelector('.sidebar');
+ const menu = document.querySelector('.menu-icon')
+  const close = document.querySelector('.close-icon')
+  //form element
+  const form = document.getElementById('contactForm');
 
 const videoList = [video1,video2,video3];
 
@@ -11,9 +20,51 @@ videoList.forEach(function(video){
     video.addEventListener('mouseover' , function(){
         video.play();
         hoverSign.classList.add("active");
-    })
+    });
     video.addEventListener('mouseout' , function(){
         video.pause();
         hoverSign.classList.remove("active");
-    })
-})
+    });
+});
+
+//side-bar functionality
+
+menu.addEventListener("click",function(){
+    sideBar.classList.remove("close-sidebar");
+    sideBar.classList.add("open-sidebar");
+});
+close.addEventListener("click",function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+});
+
+//contact button functionality
+contactBtn.forEach(function(con){
+    con.addEventListener("click",function(){
+    contactSection.scrollIntoView({behavior: "smooth"});
+});
+});
+// form validation
+from.addEventListener('submit', function(e) {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (name === '' || email === '' || message === '') {
+        e.preventDefault();
+        alert("Please fill in all the fields.");
+        return;
+    }
+
+    if (!emailPattern.test(email)) {
+        e.preventDefault();
+        alert("Please enter a valid email address.");
+        return;
+    }
+});
+
+
+
+
